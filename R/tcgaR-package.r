1eded109cdb3820ab67cf7db773b4c451b5abe7a
slee94 <stephendongsoo.lee@mail.utoronto.ca>
2016-12-06 00:58:58 -0800

    finish hw09

---
+++ R/tcgaR-package.r
@@ -0,0 +1,10 @@
+#' tcgaR:
+#'
+#' Cancer is hallmarked by many mutations that vary across different types.
+#' TCGA is a large-scale effort in bringing together genomic data on all cancer types;
+#' this package will use mutation data to produce preliminary plots to gain basic insights
+#' into our genes of interest.
+#'
+#' @name tcgaR
+#' @docType package
+NULL
